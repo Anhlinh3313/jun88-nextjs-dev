d4381af1149ebe145db801e15d39d0fa9c3daaaf
Dee <deeit789@gmail.com>
2022-09-07 16:28:10 +0700

    Dee

--- pages/index.js
+++ pages/index.js
@@ -10,34 +10,6 @@ import { useEffect, useState } from "react";
 SwiperCore.use([Autoplay]);
 
 export default function Home() {
-
-  const [data, setData] = useState([]);
-
-  const [chromeWidth, setChromeWidth] = useState(
-    typeof window !== "undefined" ? window.innerWidth : 0
-  );
-
-  // const getLink = async () => {
-  //   try {
-  //     const res = await fetch(`https://api.jun88.vin/Link/GetLink`);
-  //     const data = await res.json();
-  //     console.log(data);
-  //     setData(data)
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // };
-
-  // useEffect(() => {
-  //   getLink()
-  // }, []);
-
-  useEffect(() => {
-    window.addEventListener("resize", () => {
-      setChromeWidth(window.innerWidth);
-    });
-  }, [typeof window !== "undefined" && window]);
-
   return (
     <div>
       <Head>
@@ -45,7 +17,8 @@ export default function Home() {
         <meta name="Thông tin tổng đài" content="Thông tin tổng đài" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <>{chromeWidth > 1050 ? <HomePC /> : <HomeApp />} </>;
+      <HomePC />
+      {/* <>{chromeWidth > 1050 ? <HomePC /> : <HomeApp />} </>; */}
     </div>
   );
 }
